d858715ce06662139b5c082e2d869a4b3fa3c988
SajibBarua-art <sajibbaruanstu@gmail.com>
2021-04-02 13:31:40 +0600

    checkout

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const ObjectId = require('mongodb').ObjectID;
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port = 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ykirh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productCollection = client.db("ChainSmoker").collection("products");
+  const ordersCollection = client.db("ChainSmoker").collection("orders");
+
+
+  app.post('/addProducts', (req, res) => {
+    const products = req.body;
+    productCollection.insertMany(products)
+    .then(result => {
+      console.log(result);
+      res.send(result.insertedCount);
+    })
+  })
+
+  app.get('/products', (req, res) => {
+    productCollection.find({})
+    .toArray((err, documents) => {
+    	console.log(documents);
+    	res.send(documents);
+    })
+  })
+  
+  app.get('/product/:id', (req, res) => {
+        productCollection.find({_id: ObjectId(req.params.id)})
+        .toArray( (err, documents) => {
+            res.send(documents[0]);
+        })
+    })
+    
+    app.post('/addOrder', (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order)
+    .then(result => {
+      res.send(result.insertedCount > 0);
+    })
+  })
+  
+});
+
+app.get('/', (req,res) => {
+	res.send("Hello World");
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
